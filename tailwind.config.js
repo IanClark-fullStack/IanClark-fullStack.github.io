811f78b51684c14d7a92133e4b7852e01032f0b0
Ian Clark <IanClark2021@u.northwestern.edu>
2021-12-18 17:50:56 -0600

    Converted to REACT App

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,44 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-      display: ['courier-prime', 'Courier', 'monospace'],
-      body: ['Open Sans', 'sans-serif']
+  content: ["./src/**/*.{html,js}"],
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    // purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      fontFamily: {
+        display: ['ballinger-mono', 'monospace'],
+        bold: ['ballinger-mono-bold', 'monospace'],
+        body: ['Open Sans', 'sans-serif']
+      },
+      extend: {
+        colors: {
+          blue: '#291beb',
+          bright: '#f36ef5'
+        }, 
+        height: {
+          '128': '38rem',
+        },
+        cursor: {
+          'fancy': "url('/src/images/arrow.png')",
+        },
+        backgroundImage: {
+          'browserPattern': "url('/src/components/Projects/assets/browserBrush.png')",
+          'muskyTweets-pattern': "url('/src/components/Projects/assets/newNewsStock.jpg')",
+          'profile-MacOS': "url('/src/components/About/assets/about-mac-os9ui.jpg')",
+          'budget-tracker': "url('/src/components/Projects/assets/budgetTracker.jpg')",
+          'company-cms': "url('/src/components/Projects/assets/companyCms.jpg')",
+          'ecommerce': "url('/src/components/Projects/assets/eCommerce.jpg')",
+          'spotify-api': "url('/src/components/Projects/assets/spotifyApi.jpg')",
+          'fitness-tracker': "url('/src/components/Projects/assets/fitness-tracker.jpg')",
+          'book-search': "url('/src/components/Projects/assets/bookSearch.jpg')",
+          'dnd': "url('/src/components/Projects/assets/dnd.jpg')",
+          // 'password-Pattern': "url('/public/passWordGenerator.jpg')",
+          // 'todo-pattern': "url('/public/localStorageTodoList.jpg')",
+        }
+      },
     },
-    extend: {
-      colors: {
-        blue: '#291beb',
-        bright: '#f36ef5'
-      }, 
-      backgroundImage: {
-        'browser-pattern': "url('/assets/images/browserBrush.png')",
-        'muskyTweets-pattern': "url('/assets/images/newNewsStock.jpg')",
-        'password-Pattern': "url('/assets/images/passWordGenerator.jpg')",
-        'todo-pattern': "url('/assets/images/localStorageTodoList.jpg')",
-
-      }
+    variants: {
+      extend: {},
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
+  
